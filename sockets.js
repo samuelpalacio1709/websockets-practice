dcb00925e6b4e2953462fe3124f0b0327421b58f
Samuel Palacio <82546723+samuelpalacio1709@users.noreply.github.com>
2023-10-05 15:04:05 -0500

    fix room bug

--- sockets.js
+++ sockets.js
@@ -2,14 +2,15 @@ let ReadyPlayerCount=0;
 function listen(io){
     const pongNameSpace= io.of('/pong')
     console.log('Setting...')
-    let room;
+   
     pongNameSpace.on('connection', (socket)=>{
+        let room;
         console.log('A user has connected!', socket.id);
-
         socket.on('ready', ()=>{
             room= 'room' + Math.floor(ReadyPlayerCount/2)
+            
             socket.join(room);    
-            console.log('Player ready', socket.id)
+            console.log('Player ready', socket.id , ' In room ', room)
             ReadyPlayerCount++;
             if(ReadyPlayerCount % 2===0){
                 pongNameSpace.in(room).emit('startGame', socket.id)
@@ -22,6 +23,7 @@ function listen(io){
         })
     
         socket.on('ballMove', (ballData)=>{
+            console.log('Sending Info Ball to ', room )
             socket.to(room).emit('ballMove', ballData);
         })
         
